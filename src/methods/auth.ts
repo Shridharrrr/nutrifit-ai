2df3a1dfad3af6c582d9f513a8a7b4bd553cb0b6
Shridhar Mandrekar <geekyshridhar04@gmail.com>
2025-04-22 09:20:55 +0530

    fix: store deleted

--- src/methods/auth.ts
+++ src/methods/auth.ts
@@ -1,39 +1,23 @@
-import { auth, db, googleProvider } from "../config/firebase"
-import { onAuthStateChanged, signOut, User, createUserWithEmailAndPassword, signInWithPopup, signInWithEmailAndPassword } from "firebase/auth"
-import { getDoc, doc } from 'firebase/firestore'
-import userStore from "@/store/userStore"
-
-export const listenToAuthChanges = () => {
-    onAuthStateChanged(auth, async(user:User | null) => {
-        if (user) {
-            const docRef = doc(db,'users',user.uid)
-            const docSnap = await getDoc(docRef)
-
-            if (docSnap.exists()){
-                userStore.getState().setUser({
-                    uid: user.uid,
-                    ...docSnap.data()
-                })
-            }
-        } else {
-            userStore.getState().clearUser()
-        }
-    })
-}
+import { auth, googleProvider } from "../config/firebase";
+import {
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 export const LogoutUser = async () => {
-    await signOut(auth)
-    userStore.getState().clearUser()
-}
+  await signOut(auth);
+};
 
-export const SignupUser = async ( email:string, password:string ) => {
-      await createUserWithEmailAndPassword(auth, email, password);
-  };
+export const SignupUser = async (email: string, password: string) => {
+  await createUserWithEmailAndPassword(auth, email, password);
+};
 
-export const SigninUser = async ( email:string, password:string ) => {
-      await signInWithEmailAndPassword(auth, email, password);
-};  
+export const SigninUser = async (email: string, password: string) => {
+  await signInWithEmailAndPassword(auth, email, password);
+};
 
 export const GoogleSignupUser = async () => {
-      await signInWithPopup(auth, googleProvider);
-  };
+  await signInWithPopup(auth, googleProvider);
+};
